4f48e847df0bae8e9083c16f9fa48ef7123ba461
choycoy <passionfruits479@gmail.com>
2025-05-05 15:17:56 +0900

    feat: 반응형 로그인 UI 구현
    - 아이디 / 비밀번호 미 입력 제출 시 에러 메세지 노출
    - 로그인 실패 시 사용자에게 모달을 통해 `"로그인에 실패했습니다. 아이디 또는 비밀번호를 확인해주세요."` 메시지 제공
    - 키보드 Enter 입력으로 로그인 기능 트리거
    - 로그인된 사용자는 로그인 페이지에 접근할 수 없도록 **Public Route 접근 제어 로직 적용**

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout";
 import { lazy, Suspense } from "react";
+import PublicRoute from "./PublicRoute";
 
 const Home = lazy(() => import("@/pages/Home"));
+const Login = lazy(() => import("@/pages/Login"));
 const Router = () => {
   return (
     <Layout>
       <Suspense fallback={<div />}>
         <Routes>
           <Route index path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
         </Routes>
       </Suspense>
     </Layout>
--- src/constant.ts
+++ src/constant.ts
@@ -5,3 +5,8 @@ export const MENU_LIST = [
   { id: 0, item: "로그인", path: "/login" },
   { id: 1, item: "커뮤니티", path: "/" },
 ];
+export const ERROR_MESSAGES = {
+  idRequired: "아이디를 입력해주세요.",
+  pwdRequired: "비밀번호를 입력해주세요.",
+  loginFailed: "로그인에 실패했습니다.\n아이디 또는 비밀번호를 다시 확인해주세요.",
+};
