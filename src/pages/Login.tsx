a7376d1bd42781042566d0a85775c975d6171975
choycoy <passionfruits479@gmail.com>
2025-05-05 18:41:35 +0900

    feat: 로그인 input 필드에 label 태그 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,31 +14,44 @@ export default function Login() {
         <p className="text-gray-06 mb-4 leading-[24px] font-semibold tracking-[-0.048px]">
           로그인을 통해 더 많은 기능을 이용하세요
         </p>
-        <input
-          type="text"
-          className={`input ${idMsg ? "border-error mb-2 border-2" : "mb-3"}`}
-          onChange={(e) => {
-            setInput((prev) => ({ ...prev, id: e.target.value }));
-            if (idMsg) setError((prev) => ({ ...prev, idMsg: "" }));
-          }}
-          value={id}
-          placeholder="아이디를 입력해주세요."
-          onKeyDown={handleKeyDown}
-        />
-        {idMsg && <p className="error-msg mb-3">{idMsg}</p>}
-        <input
-          type="password"
-          className={`input ${pwdMsg ? "border-error mb-2 border-2" : "mb-3"}`}
-          onChange={(e) => {
-            setInput((prev) => ({ ...prev, pwd: e.target.value }));
-            if (pwdMsg) setError((prev) => ({ ...prev, pwdMsg: "" }));
-          }}
-          value={pwd}
-          placeholder="비밀번호를 입력해주세요."
-          onKeyDown={handleKeyDown}
-        />
-        {pwdMsg && <p className="error-msg">{pwdMsg}</p>}
-        <button onClick={handleLogin} className={`mt-6 h-[59px] w-full cursor-pointer rounded-xl bg-black text-white`}>
+        <div>
+          <label htmlFor="id" className="sr-only">
+            아이디
+          </label>
+          <input
+            id="id"
+            type="text"
+            className={`input ${idMsg ? "border-error mb-2 border-2" : "mb-3"}`}
+            onChange={(e) => {
+              setInput((prev) => ({ ...prev, id: e.target.value }));
+              if (idMsg) setError((prev) => ({ ...prev, idMsg: "" }));
+            }}
+            value={id}
+            placeholder="아이디를 입력해주세요."
+            onKeyDown={handleKeyDown}
+          />
+          {idMsg && <p className="error-msg mb-3">{idMsg}</p>}
+        </div>
+        <div>
+          <label htmlFor="pwd" className="sr-only">
+            비밀번호
+          </label>
+          <input
+            id="pwd"
+            type="password"
+            className={`input ${pwdMsg ? "border-error mb-2 border-2" : "mb-3"}`}
+            onChange={(e) => {
+              setInput((prev) => ({ ...prev, pwd: e.target.value }));
+              if (pwdMsg) setError((prev) => ({ ...prev, pwdMsg: "" }));
+            }}
+            value={pwd}
+            placeholder="비밀번호를 입력해주세요."
+            onKeyDown={handleKeyDown}
+          />
+          {pwdMsg && <p className="error-msg">{pwdMsg}</p>}
+        </div>
+
+        <button onClick={handleLogin} className={`black-btn mt-6 h-[59px] w-full cursor-pointer rounded-xl`}>
           로그인
         </button>
       </section>
