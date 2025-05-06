b8bc2f3669f46cc888366240b476f25d1d24b6bc
choycoy <passionfruits479@gmail.com>
2025-05-06 20:02:29 +0900

    design: 모바일 게시판 스타일링 조정

--- src/components/home/PostList.tsx
+++ src/components/home/PostList.tsx
@@ -6,15 +6,7 @@ import { useNavigate } from "react-router-dom";
 export default function PostList({ postList }: { postList: Post[] | undefined }) {
   const navigate = useNavigate();
   if (!postList) return null;
-  const PostInfo = ({ createdAt, commentCount }: { createdAt: string; commentCount: number }) => (
-    <>
-      <p>{dayjs(createdAt).format("YY.MM.DD")}</p>
-      <div className="flex items-center gap-x-1">
-        <img alt="댓글 아이콘" src={commentIcon} className="tab:w-[22px] tab:h-[22px] h-5 w-5" />
-        <p className="leading-[24px] tracking-[-0.048px]">{commentCount}</p>
-      </div>
-    </>
-  );
+
   return (
     <>
       {postList.map((post: Post) => {
@@ -28,12 +20,11 @@ export default function PostList({ postList }: { postList: Post[] | undefined })
             <p className="tab:font-normal tab:text-lg tab:leading-[27px] tab:tracking-[-0.054px] line-clamp-1 text-base leading-[24px] font-semibold tracking-[-0.048px]">
               {title}
             </p>
-            <div className="text-gray-06 tab:leading-[24px] tab:tracking-[-0.048px] tab:text-base tab:justify-baseline flex items-center justify-between gap-x-2 text-sm leading-[21px] tracking-[-0.042px]">
-              <div className="tab:flex hidden items-center gap-x-2">
-                <PostInfo createdAt={createdAt} commentCount={commentCount} />
-              </div>
-              <div className="tab:hidden flex items-center gap-x-2">
-                <PostInfo createdAt={createdAt} commentCount={commentCount} />
+            <div className="text-gray-06 tab:leading-[24px] tab:tracking-[-0.048px] tab:text-base tab:justify-baseline flex w-full items-center justify-between gap-x-2 text-sm leading-[21px] tracking-[-0.042px]">
+              <p>{dayjs(createdAt).format("YY.MM.DD")}</p>
+              <div className="flex items-center gap-x-1">
+                <img alt="댓글 아이콘" src={commentIcon} className="tab:w-[22px] tab:h-[22px] h-5 w-5" />
+                <p className="leading-[24px] tracking-[-0.048px]">{commentCount}</p>
               </div>
               <div className="flex items-center gap-x-2">
                 <div className="tab:bg-gray-06 bg-gray-04 h-6 w-6 rounded-full" />
