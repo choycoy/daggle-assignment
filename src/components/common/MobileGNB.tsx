1d855c2bac83a2bd32c70deb4382bad930c6605a
choycoy <passionfruits479@gmail.com>
2025-05-06 17:34:47 +0900

    feat: 헤더에 홈 경로에 따른 메뉴 및 뒤로가기 아이콘 추가

--- src/components/common/MobileGNB.tsx
+++ src/components/common/MobileGNB.tsx
@@ -1,15 +1,26 @@
 import menuIcon from "@/assets/icons/menu.svg";
 import { useState } from "react";
 import Drawer from "./Drawer";
+import { useLocation, useNavigate } from "react-router-dom";
+import chevronLeft from "@/assets/icons/chevron-left-gray09.svg";
 
 export default function MobileGNB() {
   const [showDrawer, setShowDrawer] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isHome = location.pathname === "/";
 
   return (
     <header className="fixed top-0 left-0 z-50 h-14 w-full bg-white py-4 pl-4">
-      <button onClick={() => setShowDrawer(true)} aria-label="메뉴 열기">
-        <img src={menuIcon} alt="메뉴 아이콘" className="h-6 w-6" />
-      </button>
+      {isHome ? (
+        <button onClick={() => setShowDrawer(true)} aria-label="메뉴 열기">
+          <img src={menuIcon} alt="메뉴 아이콘" className="h-6 w-6" />
+        </button>
+      ) : (
+        <button onClick={() => navigate(-1)} aria-label="뒤로가기">
+          <img src={chevronLeft} alt="뒤로가기 아이콘" className="h-6 w-6" />
+        </button>
+      )}
       {showDrawer && <Drawer setShowDrawer={setShowDrawer} />}
     </header>
   );
