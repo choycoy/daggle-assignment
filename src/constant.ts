0c0a8b915ad942c831ece6446d2d839bdb44061d
choycoy <passionfruits479@gmail.com>
2025-05-07 17:52:31 +0900

    fix: loginRequired 에러 메시지 수정

--- src/constant.ts
+++ src/constant.ts
@@ -14,7 +14,7 @@ export const ERROR_MESSAGES = {
   idRequired: "아이디를 입력해주세요.",
   pwdRequired: "비밀번호를 입력해주세요.",
   loginFailed: "로그인에 실패했어요.\n아이디 또는 비밀번호를 다시 확인해주세요.",
-  loginRequired: "로그인 후 이용하실 수 있어요",
+  loginRequired: "로그인 후 이용하실 수 있어요.",
   refreshRequired: "로그인 세션이 만료되었어요. 다시 로그인해 주세요.",
   deleteForbiddenMsg: "댓글 작성자만 삭제할 수 있어요.",
   editForbiddenMsg: "댓글 작성자만 수정할 수 있어요.",
