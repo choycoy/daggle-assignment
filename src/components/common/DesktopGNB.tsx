d2f195dddfc5f217802582b0f305963849317845
choycoy <passionfruits479@gmail.com>
2025-05-08 15:34:39 +0900

    design: 데스크톱 환경에서 최대 넓이 1200px로 설정

--- src/components/common/DesktopGNB.tsx
+++ src/components/common/DesktopGNB.tsx
@@ -18,42 +18,44 @@ export default function DesktopGNB() {
   };
 
   return (
-    <header className="shadow-header border-gray-03 fixed top-0 left-0 z-50 flex h-[86px] w-full items-center justify-between border-b bg-white px-[120px]">
-      <Link to="/" onClick={handleLogoClick}>
-        <img src={LogoImg} alt="다글제작소 로고" className="h-6 w-[120px]" />
-      </Link>
-      {isLoggedIn ? (
-        <button
-          type="button"
-          onClick={() => setShowNickname(!showNickname)}
-          aria-label="프로필 보기"
-          className="cursor-pointer"
-        >
-          <img src={profileIcon} alt="프로필 아이콘" />
-        </button>
-      ) : (
-        <button
-          type="button"
-          className="cursor-pointer text-lg leading-[32px] font-semibold whitespace-nowrap"
-          onClick={() => navigate("/login")}
-        >
-          로그인
-        </button>
-      )}
-      {showNickname && (
-        <div className="shadow-nickname fixed top-[105px] right-[120px] flex items-center gap-x-3 rounded-xl bg-white px-6 py-5">
-          {user?.profileImageUrl ? (
-            <img
-              src={user.profileImageUrl}
-              alt={`${user?.nickname ?? "다글다글"} 님의 프로필 이미지`}
-              className="h-8 w-8 rounded-full"
-            />
-          ) : (
-            <div className="bg-gray-06 h-8 w-8 rounded-full" />
-          )}
-          <p className="text-lg leading-[1.5] font-semibold">{user?.nickname ?? "다글다글"} 님</p>
-        </div>
-      )}
+    <header className="shadow-header border-gray-03 fixed top-0 left-0 z-50 h-[86px] w-full border-b bg-white px-[120px]">
+      <div className="desktop:mx-auto flex h-full max-w-[1200px] items-center justify-between">
+        <Link to="/" onClick={handleLogoClick}>
+          <img src={LogoImg} alt="다글제작소 로고" className="h-6 w-[120px]" />
+        </Link>
+        {isLoggedIn ? (
+          <button
+            type="button"
+            onClick={() => setShowNickname(!showNickname)}
+            aria-label="프로필 보기"
+            className="cursor-pointer"
+          >
+            <img src={profileIcon} alt="프로필 아이콘" />
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="cursor-pointer text-lg leading-[32px] font-semibold whitespace-nowrap"
+            onClick={() => navigate("/login")}
+          >
+            로그인
+          </button>
+        )}
+        {showNickname && (
+          <div className="shadow-nickname fixed top-[105px] right-[120px] flex items-center gap-x-3 rounded-xl bg-white px-6 py-5">
+            {user?.profileImageUrl ? (
+              <img
+                src={user.profileImageUrl}
+                alt={`${user?.nickname ?? "다글다글"} 님의 프로필 이미지`}
+                className="h-8 w-8 rounded-full"
+              />
+            ) : (
+              <div className="bg-gray-06 h-8 w-8 rounded-full" />
+            )}
+            <p className="text-lg leading-[1.5] font-semibold">{user?.nickname ?? "다글다글"} 님</p>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -15,10 +15,10 @@ export default function Layout({ children }: { children: ReactNode }) {
   const isPostForm = location.pathname === "/post/write";
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="tab:bg-gray-01 tab:items-center tab:justify-center flex min-h-screen w-full flex-col">
       {isMobile && !isPostForm && <MobileGNB />}
       {!isMobile && <DesktopGNB />}
-      <main className="tab:bg-gray-01 desktop:px-[120px] tab:pt-[86px] tab:px-[30px] h-full flex-1 pt-14">
+      <main className="tab:bg-gray-01 desktop:px-[120px] tab:pt-[86px] tab:px-[30px] desktop:max-w-[1440px] h-full w-full flex-1 pt-14">
         {children}
       </main>
     </div>
