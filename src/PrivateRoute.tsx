f97619007559311606bc0de87330a35d28779755
choycoy <passionfruits479@gmail.com>
2025-05-08 18:12:05 +0900

    fix: 로그인 상태 초기화 액세스 토큰값으로 설정

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -9,7 +9,6 @@ export default function PrivateRoute() {
   const isLoginPage = location.pathname === "/login" && location.search === "";
   const cameFromOtherPage = location.state?.from || new URLSearchParams(location.search).get("from");
 
-  if (isLoggedIn === null) return;
   if (!isLoggedIn && !isLoginPage && !cameFromOtherPage) {
     alert(UI_ERRORS.LOGIN_REQUIRED);
     return <Navigate to="/login" replace state={{ from: location.pathname }} />;
--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,18 +1,23 @@
 import { create } from "zustand";
 import { User } from "@/types/interface";
+import { STORAGE_KEYS } from "@/constant";
 
 interface UserState {
   user: User | null;
   setUser: (user: User | null) => void;
-  isLoggedIn: boolean | null;
+  isLoggedIn: boolean;
   setIsLoggedIn: (isLoggedIn: boolean) => void;
   clearUser: () => void;
 }
 
-export const useAuthStore = create<UserState>((set) => ({
-  user: null,
-  isLoggedIn: null,
-  setUser: (user) => set({ user }),
-  setIsLoggedIn: (isLoggedIn) => set({ isLoggedIn: isLoggedIn }),
-  clearUser: () => set({ user: null }),
-}));
+export const useAuthStore = create<UserState>((set) => {
+  const accessToken = localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN_KEY);
+
+  return {
+    user: null,
+    isLoggedIn: !!accessToken,
+    setUser: (user) => set({ user }),
+    setIsLoggedIn: (isLoggedIn) => set({ isLoggedIn }),
+    clearUser: () => set({ user: null }),
+  };
+});
