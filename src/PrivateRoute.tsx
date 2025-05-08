af31ceaa4bb3c16bfa801870338b8aaeb0d9535c
choycoy <passionfruits479@gmail.com>
2025-05-08 15:17:19 +0900

    refactor: 인증된 사용자만 접근 가능한 PrivateRoute 리팩토링
    - 로그인되지 않은 사용자가 인증이 필요한 페이지에 접근할 경우 로그인 페이지로 리디렉션
    - 로그인 상태에서 로그인 페이지로 접근하면 홈 페이지로 리디렉션
    - 로그인 페이지로 리디렉션 시, 이전 페이지 경로를 `from` 쿼리 파라미터로 전달

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,9 +1,19 @@
 import { useAuthStore } from "@/store/useAuthStore";
-import { Navigate } from "react-router-dom";
-import { JSX } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { UI_ERRORS } from "./constant";
 
-export default function PrivateRoute({ children }: { children: JSX.Element }) {
+export default function PrivateRoute() {
   const { isLoggedIn } = useAuthStore();
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login" && location.search === "";
+  const cameFromOtherPage = location.state?.from || new URLSearchParams(location.search).get("from");
 
-  return isLoggedIn ? <Navigate to="/" replace /> : children;
+  if (!isLoggedIn && !isLoginPage && !cameFromOtherPage) {
+    alert(UI_ERRORS.loginRequired);
+    return <Navigate to="/login" replace state={{ from: location.pathname }} />;
+  }
+  if (isLoggedIn && isLoginPage && !cameFromOtherPage) return <Navigate to="/" replace />;
+
+  return <Outlet />;
 }
