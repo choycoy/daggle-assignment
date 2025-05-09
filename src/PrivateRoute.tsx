5d5e0c47ba021c95a91c0e69389a017b629c382f
choycoy <passionfruits479@gmail.com>
2025-05-09 11:57:29 +0900

    fix: 퍼블릭 페이지에서 alert 제거

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,6 +1,7 @@
 import { useAuthStore } from "@/store/useAuthStore";
 import { Navigate, useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { UI_ERRORS } from "./constant";
 
 export default function PrivateRoute() {
   const { isLoggedIn } = useAuthStore();
@@ -8,6 +9,10 @@ export default function PrivateRoute() {
   const isLoginPage = location.pathname === "/login" && location.search === "";
   const cameFromOtherPage = location.state?.from || new URLSearchParams(location.search).get("from");
 
+  if (!isLoggedIn && !isLoginPage && !cameFromOtherPage) {
+    alert(UI_ERRORS.LOGIN_REQUIRED);
+    return <Navigate to="/login" replace state={{ from: location.pathname }} />;
+  }
   if (isLoggedIn && isLoginPage && !cameFromOtherPage) return <Navigate to="/" replace />;
 
   return <Outlet />;
